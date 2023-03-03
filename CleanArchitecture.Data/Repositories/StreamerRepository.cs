60d7939f05f6db95ac7fb5fca2bd06a0f07850da
Nataly Vasquez <nvalbitres@gmail.com>
2023-03-02 22:43:25 -0500

    Implementation StreamerRepository and VideoRepository

---
+++ CleanArchitecture.Data/Repositories/StreamerRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class StreamerRepository : RepositoryBase<Streamer>, IStreamerRepository
+    {
+        // implementar el constructor que inicialice la cadena de conexion e instancie DBContext
+        public StreamerRepository(StreamerDbContext context) : base(context)
+        {
+            // base inicializarlo desde el padre que es RepositoryBase
+        }
+    }
+}
---
+++ CleanArchitecture.Data/Repositories/VideoRepository.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class VideoRepository : RepositoryBase<Video>, IVideoRepository
+    {
+        public VideoRepository(StreamerDbContext context) : base(context)
+        {
+            // va a recibir un dbcontext que se va a setear en el padre base (RepositoryBase)
+        }
+
+        public async Task<Video> GetVideoByName(string nameVideo)
+        {
+            return await _context.Videos!.Where(o => o.Nombre == nameVideo).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Video>> GetVideoByUsername(string username)
+        {
+            return await _context.Videos!.Where(v=> v.CreatedBy==username).ToListAsync();
+        }
+    }
+}
